88e402c946f5b34eee31010cfe089491c81ee9ce
Kordell Parrish <parrishkordell@gmail.com>
2020-08-07 07:25:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,2 +1,68 @@
+def hopper
+    programmer_hash = 
+         {
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+programmer_hash[:grace_hopper]
+
+end
+
+def alan_kay_is_known_for
+    # What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+
+    programmer_hash = 
+         {
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+     programmer_hash[:alan_kay][:known_for]
+end
+
+def dennis_ritchies_language
+    programmer_hash = 
+         {
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+     programmer_hash[:dennis_ritchie][:languages].join
+end
+
+def adding_matz
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
 
 
